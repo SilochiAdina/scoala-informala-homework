9e055550f9b6efd3fa778b5aec0b0fb73f4cfd5b
Adina <silochiadinanicoleta@gmail.com>
2019-03-28 22:25:57 +0200

    a doua modificare

---
+++ tema09ian/Temperature/src/Temperature.java
@@ -0,0 +1,34 @@
+/**
+ * @author: Silochi Adina
+ * In this code was created a method to convert tempereture from Fahrenheit to Celsius.
+ *Then was writed a program to print body temperature, measured in Celsius.
+ * You need to write the temperature in Fahrenheit degrees.
+ */
+
+import java.util.Scanner;
+
+public class Temperature {
+
+        public static void main(String[] args) {
+
+            System.out.println("Enter Fahrenheit value:");
+            Scanner sc = new Scanner(System.in);
+            double fah = sc.nextDouble();
+            convert(fah);
+        }
+
+        public static double convert(double fah){
+            double cel = (fah - 32) / 1.8;
+            if (cel > 37) {
+                System.out.println("You are ill!");
+            } else if (cel <= 37) {
+                System.out.println("Your body temperature in Celsius degrees is:" + cel);
+            }
+            return cel;
+
+        }
+
+
+}
+
+
